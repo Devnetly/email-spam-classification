46192cd6d467f0f1a005423ef84d7806baf0e114
abdelnour13 <ab.fellah@esi-sba.dz>
2024-02-19 15:08:25 +0100

    home page

--- streamlit/home.py
+++ streamlit/home.py
@@ -1,5 +1,16 @@
 import streamlit as st
 
-st.set_page_config(page_title="My Streamlit App", page_icon="🧊", layout="wide", initial_sidebar_state='expanded')
+st.set_page_config(page_title="SEDS PROJECT | Email Spam Classification", page_icon="🧊", layout="wide", initial_sidebar_state='expanded')
 
-st.sidebar.success("Welcome to my Streamlit App")
+# st.sidebar.success("Welcome to my Streamlit App")
+
+
+st.markdown(""" 
+# Welcome to our Streamlit app
+""")
+
+st.divider()
+
+st.markdown('#### This app uses machine learning to classify emails as spam or not spam. To get started, go to the next section in the sidebar.')
+
+st.balloons()
